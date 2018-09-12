6faaa81c8e596ca90bb35b79608c98e72475259e
RoryLetteney <rory.letteney@gmail.com>
2018-09-12 11:23:10 -0500

    Added number labels to inputs

--- src/components/input.js
+++ src/components/input.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Input = ({title, state, name}, onChange) => {
+const Input = ({title, state, name}, onChange, index) => {
     return (
-        <div className='input'>
-            <input key ={name} name ={name} value={state} onChange={onChange}/>
-            <label>{title}</label>
+        <div  key ={index} className='input'>
+            <label className="input__number">{index + 1}</label>
+            <input name ={name} value={state} onChange={onChange}/>
+            <label className="input__title">{title}</label>
         </div>
     )
 }
