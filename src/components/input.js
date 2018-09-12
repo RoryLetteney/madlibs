1c74275a7fa3400142e3aa604a611da9d56e9f4d
RoryLetteney <rory.letteney@gmail.com>
2018-09-12 09:42:03 -0500

    Modified handleInputChange to handle multiple input cases

--- src/components/input.js
+++ src/components/input.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Input = (label) => {
+const Input = (label, state, onChange, name) => {
     return (
         <div className='input'>
-            <input />
+            <input name ={name} value={state} onChange={onChange}/>
             <label>{label}</label>
         </div>
     )
