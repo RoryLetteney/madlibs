2a2f047634f17a509319e311ec0efbec6675c9de
RoryLetteney <rory.letteney@gmail.com>
2018-09-12 19:34:59 -0500

    Added header h1 and p styles

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className='Header'>
+        <div className='header'>
             <div className='header__skew'></div>
             <div className='header__border'></div>
             <h1>Bottega Madlibs</h1>
